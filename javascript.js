36228d948d168d7d049fa9038e238afacd287b9a
soundar-1998 <soundark221@gmail.com>
2024-10-30 16:40:00 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+let count=10;
+ function like(){
+    count=count+1;//count++;
+    alert(`hey!! you  got  ${count} like`);
+    alert(`hey !! you got this  likes`,count);
+ }
+ let subcribe=20;
+  function subc(){
+    document.write(`you  got ${ subcribe+1} subcribes`);
+  } 
+  function dodo(){
+    document.getElementById('demo').innerHTML=new Date();
+  }
---
+++ script.js
@@ -0,0 +1,2 @@
+let a1= Math.random()*10000
+console.log(Math.floor(a1));    
